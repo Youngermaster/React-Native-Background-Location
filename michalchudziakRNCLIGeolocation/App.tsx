a533c64535ea6003801e167ab6e779e0fb33670a
Juan Manuel Young Hoyos <vndrzrfshJXY@wegmans.com>
2024-04-12 17:55:21 -0500

    Refactor App.tsx to remove unused imports and update navigation structure

--- michalchudziakRNCLIGeolocation/App.tsx
+++ michalchudziakRNCLIGeolocation/App.tsx
@@ -6,9 +6,7 @@
  */
 
 import React from 'react';
-import {SafeAreaView, useColorScheme} from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import * as Screens from './src/screens';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
@@ -16,22 +14,14 @@ import {createStackNavigator} from '@react-navigation/stack';
 const Stack = createStackNavigator();
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={Screens.HomeScreen} />
-          <Stack.Screen name="Examples" component={Screens.ExamplesScreen} />
-          <Stack.Screen name="TestCases" component={Screens.TestCasesScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Screens.HomeScreen} />
+        <Stack.Screen name="Examples" component={Screens.ExamplesScreen} />
+        <Stack.Screen name="TestCases" component={Screens.TestCasesScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
