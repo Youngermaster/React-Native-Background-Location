f5048f64aedb5ff76b75af69ec649530998ff76e
Juan Manuel Young Hoyos <vndrzrfshJXY@wegmans.com>
2024-04-12 20:24:45 -0500

    Refactor React Native Geolocation imports and remove unused code

--- RNCLIExpoIntegration/App.tsx
+++ RNCLIExpoIntegration/App.tsx
@@ -1,10 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
