b7209144bd30683ba84321d4274a00d3c326535f
Juan Manuel Young Hoyos <vndrzrfshJXY@wegmans.com>
2025-01-03 14:56:43 -0500

    feat(app): obytes starter added

---
+++ ObytesStarterMapTest/app.config.ts
@@ -0,0 +1,83 @@
+/* eslint-disable max-lines-per-function */
+import type { ConfigContext, ExpoConfig } from '@expo/config';
+import type { AppIconBadgeConfig } from 'app-icon-badge/types';
+
+import { ClientEnv, Env } from './env';
+
+const appIconBadgeConfig: AppIconBadgeConfig = {
+  enabled: Env.APP_ENV !== 'production',
+  badges: [
+    {
+      text: Env.APP_ENV,
+      type: 'banner',
+      color: 'white',
+    },
+    {
+      text: Env.VERSION.toString(),
+      type: 'ribbon',
+      color: 'white',
+    },
+  ],
+};
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: Env.NAME,
+  description: `${Env.NAME} Mobile App`,
+  owner: Env.EXPO_ACCOUNT_OWNER,
+  scheme: Env.SCHEME,
+  slug: 'obytesapp',
+  version: Env.VERSION.toString(),
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'automatic',
+  newArchEnabled: true,
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: Env.BUNDLE_ID,
+  },
+  experiments: {
+    typedRoutes: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#2E3C4B',
+    },
+    package: Env.PACKAGE,
+  },
+  web: {
+    favicon: './assets/favicon.png',
+    bundler: 'metro',
+  },
+  plugins: [
+    [
+      'expo-splash-screen',
+      {
+        backgroundColor: '#2E3C4B',
+        image: './assets/splash-icon.png',
+        imageWidth: 150,
+      },
+    ],
+    [
+      'expo-font',
+      {
+        fonts: ['./assets/fonts/Inter.ttf'],
+      },
+    ],
+    'expo-localization',
+    'expo-router',
+    ['app-icon-badge', appIconBadgeConfig],
+    ['react-native-edge-to-edge'],
+  ],
+  extra: {
+    ...ClientEnv,
+    eas: {
+      projectId: Env.EAS_PROJECT_ID,
+    },
+  },
+});
